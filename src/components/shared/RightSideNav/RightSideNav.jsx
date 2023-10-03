1a8518c72e29785998cb766bc711a61daa539766
Anupama_Halder <anupamahalder2022@gmail.com>
2023-10-03 21:27:48 +0530

    Added a div for login with google and github

--- src/components/shared/RightSideNav/RightSideNav.jsx
+++ src/components/shared/RightSideNav/RightSideNav.jsx
@@ -1,8 +1,20 @@
-
+import {FcGoogle} from 'react-icons/fc';
+import {FaGithub} from 'react-icons/fa';
 const RightSideNav = () => {
     return (
         <div>
-          <h2 className="text-2xl">Right Side nav</h2>  
+            {/* Login with div  */}
+            <div>
+                <h2 className="text-black font-semibold text-xl py-3">Login With</h2>
+                <button className='btn normal-case btn-outline'>
+                    <FcGoogle className='text-2xl'></FcGoogle>
+                    Login With Google
+                </button>
+                <button className='btn mt-4 normal-case btn-outline'>
+                    <FaGithub className='text-2xl'></FaGithub>
+                    Login With Github
+                </button>
+            </div> 
         </div>
     );
 };
